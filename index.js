545dbd201f2a0bac9e17c0cf2959ec2ffe9d2650
Kijun Shin <benedict02@naver.com>
2020-06-18 14:59:52 +0900

    Dispatch click event to appointed eventElement

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const url = 'https://old.reddit.com/r/programming/comments/fnjpaq/excalidraw_now
         }
     });
     await page.goto(url);
-    
+
     const client = await page.target().createCDPSession();
     const {
         root: {
@@ -33,7 +33,7 @@ const url = 'https://old.reddit.com/r/programming/comments/fnjpaq/excalidraw_now
     });
     const eventListeners = (await Promise.all(
         nodeIds.map(nodeId => {
-            let promise = client.send('DOM.resolveNode', { nodeId });
+            let promise = client.send('DOM.resolveNode', { nodeId }).catch(err => {});
             promise = promise.then(({ object: { objectId } }) => {
                 let eventListenersPromise = client.send('DOMDebugger.getEventListeners', { objectId });
                 return eventListenersPromise;
@@ -51,21 +51,30 @@ const url = 'https://old.reddit.com/r/programming/comments/fnjpaq/excalidraw_now
           .filter(eventEl => eventEl)
           .filter(eventEl => eventEl.listeners.some(({ type }) => targetEventTypes.includes(type)));
 
-    console.log(eventListeners);
+    // console.log(eventListeners);
 
-    // const elementBoxModel = (await Promise.all(
-    //     eventListeners.map(eventEl => {
-    //         let boxModel = client.send('DOM.getBoxModel', { nodeId: eventEl.nodeId });
-    //         boxModel = boxModel.then(({ model }) => { eventEl.size = model.width * model.height; return model; }).catch(err => console.log(err));
-    //         return boxModel;
-    //     })
-    // ));
-    
+    const nodeList = (await Promise.all(
+        eventListeners.map(eventEl => {
+            let node = client.send('DOM.describeNode', { nodeId: eventEl.nodeId });
+            return node;
+        })
+    ));
 
-    // for (let eventEl of eventListeners) { console.log(eventEl.listeners); } 
-    // console.log(eventListeners.length, eventListeners);
+    // dispatch event prototype
+    let temp = nodeList[2].node.attributes;
+    console.log(temp);
+    for (let i = 0; 2 * i < temp.length; i++) {
+        console.log("??");
+            if (temp[2 * i] === 'class') {
+                let classKey = temp[2 * i + 1].trim().split(" ");
+                console.log(classKey);
+                await page.evaluate(classKey => {
+                    console.log("Waiting...");
+                    document.querySelector(`.${classKey[0]}`).dispatchEvent(new Event('click'));
+                    console.log("Dispatched!");
+                }, classKey);
+            }
+        }
 
-    // sort by padding edge
-    
     await browser.close();
 })();
