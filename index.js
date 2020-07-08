0670c9ca5098bb17a8bb3e48d6fd7c38de1e6d7f
Kijun Shin <benedict02@naver.com>
2020-07-08 14:35:41 +0900

    Enable replay features

--- index.js
+++ index.js
@@ -4,23 +4,30 @@ const puppeteer = require('puppeteer');
 // nodejs module
 const path = require('path');
 const fs = require('fs/promises');
+const fileSystem = require('fs');
+const readline = require('readline');
 
 const DEBUG = !!process.env.DEBUG;
 const targetEventTypes = ['click'];
 
-const targetURL = 'https://old.reddit.com/r/programming/comments/fnjpaq/excalidraw_now_supports_real_time_end_to_end/';
+const targetURL = 'https://github.com';
 let dir = './archive/' + new URL(targetURL).hostname + new URL(targetURL).pathname;
 
-(async () => {
+async function archive() {
     await fs.mkdir(dir, { recursive: true });
     await fs.writeFile(path.join(dir, 'map.txt'), '');
     
-    const browser = await puppeteer.launch({ headless: !DEBUG });
+    const browser = await puppeteer.launch({
+        headless: !DEBUG,
+        args: [
+            '--disable-web-security'
+        ]
+    });
     const [page] = await browser.pages();
     await page.setRequestInterception(true);
 
     page.on('request', request => {
-        if (!new URL(request.url()).host.includes("reddit")) {
+        if (!new URL(request.url()).host.includes("github")) {
             request.abort();
         } else {
             request.continue();
@@ -28,9 +35,12 @@ let dir = './archive/' + new URL(targetURL).hostname + new URL(targetURL).pathna
     });
     
     page.on('response', async (response) => {
-        // console.log("url:", response.url());
+        //console.log("url:", response.url());
         const status = response.status();
-        // console.log("status:", status);
+        const resourceType = response.request().resourceType();
+        // console.log("Type:", resourceType);
+        // console.log("Request headers:", await response.request().headers());
+        // console.log("Headers:", await response.headers());
         
         if (!(status >= 200 && status < 300)) {
             console.log('Redirect from', response.url(), 'to', response.headers()['location']);
@@ -38,14 +48,18 @@ let dir = './archive/' + new URL(targetURL).hostname + new URL(targetURL).pathna
             const fileName = response.url().split('/').pop();
             const filePath = path.join(dir, fileName);
             try {
-                const buffer = await response.buffer();    
+                const buffer = await response.buffer();
                 if (fileName) {
                     await fs.writeFile(filePath.substring(0, 255), buffer);
                     await fs.appendFile(path.join(dir, 'map.txt'), await response.request().url() + " " + await fileName + "\n");
+                } else {
+                    await fs.writeFile(path.join(filePath.substring(0, 255), 'darchive-entry'), buffer);
+                    await fs.appendFile(path.join(dir, 'map.txt'), await response.request().url() + " " + "darchive-entry" + "\n");
+                    console.log("entered!");
                 }
             } catch (error) {
                 console.error(error);
-                console.error('This error originated from ', response);
+                // console.error('This error originated from ', response);
             }
         }
     });
@@ -53,7 +67,6 @@ let dir = './archive/' + new URL(targetURL).hostname + new URL(targetURL).pathna
     await page.goto(targetURL, { waitUntil: 'networkidle2' });
     const client = await page.target().createCDPSession();
 
-    
     // get all eventListeners defined by targetEvent
     const {
         root: {
@@ -84,8 +97,8 @@ let dir = './archive/' + new URL(targetURL).hostname + new URL(targetURL).pathna
             return promise;
         })
     )) // objectId: Runtime.RemoteObjectId, nodeId: DOM.NodeId
-          .filter(eventEl => eventEl)
-          .filter(eventEl => eventEl.listeners.some(({ type }) => targetEventTypes.includes(type)));
+        .filter(eventEl => eventEl)
+        .filter(eventEl => eventEl.listeners.some(({ type }) => targetEventTypes.includes(type)));
 
     const nodeList = (await Promise.all(
         eventListeners.map(eventEl => {
@@ -107,7 +120,101 @@ let dir = './archive/' + new URL(targetURL).hostname + new URL(targetURL).pathna
             }
         }
     }
+    
     setTimeout(async () => {
         await browser.close();
     }, 1500);
-})();
+    
+}
+
+async function replay() {
+    const browser = await puppeteer.launch({
+        headless: !DEBUG,
+        args: [
+            '--disable-web-security'
+        ]
+    });
+    const [page] = await browser.pages();
+    page.setRequestInterception(true);
+
+    let urlResourceMap = await readMapFile();
+    
+    page.on('request', async (request) => {
+        // console.log("request:", request);
+        const resourceURL = await request.url().split('/').pop();
+        
+        let bodyData;
+        if (!resourceURL) {
+            if (await fileSystem.existsSync(path.join(dir, 'darchive-entry'))) {
+                bodyData = await fs.readFile(path.join(dir, 'darchive-entry'));
+                console.log(bodyData);
+            } else {
+                console.error("No entry point! Please check your archive.");
+            }
+        } else {
+            if (Object.values(urlResourceMap).indexOf(await request.url().split('/').pop()) > -1) {
+                bodyData = await fs.readFile(path.join(dir, resourceURL));
+                console.log("url:", await request.url().split('/').pop());
+            } else {
+                bodyData = null;
+            }
+        }
+        
+        if (bodyData) {
+            const newHeader = Object.assign({}, await request, {
+                date: new Date().toUTCString(),
+                'content-length': bodyData.length
+            });
+            request.respond({
+                status: 200,
+                headers: newHeader,
+                body: bodyData
+            });
+        } else {
+            request.respond({
+                status: 404
+            });
+        }
+    });
+
+    await page.goto(targetURL, { waitUntil: 'domcontentloaded' });
+}
+
+function readMapFile() {
+    let urlResourceMap = {};
+    let readStream = fileSystem.createReadStream(path.join(dir, 'map.txt'));
+    let reader = readline.createInterface(readStream, process.stdout);
+
+    let promise = new Promise((resolve, reject) => {
+        reader.on('line', line => {
+            let token = line.split(' ');
+            urlResourceMap[token[0]] = token[1];
+        });
+
+        reader.on('close', () => {
+            resolve(urlResourceMap);
+        });
+    });
+    
+    return promise;
+}
+
+// archive();
+replay();
+
+async function test() {
+    const browser = await puppeteer.launch({ headless: !DEBUG });
+    const [page] = await browser.pages();
+    page.setRequestInterception(true);
+    page.on('request', req => {
+        req.respond({
+            status: 404,
+        });
+    });
+    await page.goto('https://www.google.com/');
+    setTimeout(async () => {
+        await browser.close();
+    }, 5000);
+}
+
+// test();
